692ad3d0b4b664723628b60b197056a5e3e8222a
Iaroslav Krasnokutskii <Yroslavk1995@gmail.com>
2020-08-20 13:42:29 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Ярослав on 8/20/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
